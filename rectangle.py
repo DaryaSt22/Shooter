9daa354dcb5075d07753bb1d3c71b2f5cfd98e76
Darya <ara1015@tut.by>
2025-09-01 16:00:47 +0300

    added images

---
+++ rectangle.py
@@ -0,0 +1,45 @@
+import sys
+
+import pygame
+
+# from random import randint
+
+# clock = pygame.time.Clock()
+
+pygame.init()
+
+screen_width, screen_height = 800, 600
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+pygame.display.set_caption("My Pygame")
+fill_color = (32, 52, 71)
+
+rect_width, rect_height = 100, 200
+rect_x = screen_width / 2 - rect_width / 2
+rect_y = screen_height / 2 - rect_height / 2
+rect_color = pygame.Color('lightyellow')
+
+step = 10
+
+while True:
+    for event in pygame.event.get():
+        print(event)
+        if event.type == pygame.QUIT:
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP and rect_y >= step:
+                rect_y = rect_y - step
+            if event.key == pygame.K_DOWN and rect_y <= screen_height - rect_height - step:
+                rect_y = rect_y + step
+            if event.key == pygame.K_LEFT and rect_x >= step:
+                rect_x = rect_x - step
+            if event.key == pygame.K_RIGHT and rect_x <= screen_width - rect_width - step:
+                rect_x = rect_x + step
+
+    # screen.fill(pygame.Color('blue'))
+    # screen.fill((randint(0, 255), randint(0, 255), randint(0, 255)))
+    screen.fill(fill_color)
+    pygame.draw.rect(screen, rect_color, (rect_x, rect_y, rect_width, rect_height))
+    pygame.display.update()
+    # clock.tick(5)
