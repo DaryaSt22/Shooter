ab86a94b85a59a7468682f4fca483af35ce375bf
Darya <ara1015@tut.by>
2025-09-01 13:43:42 +0300

    moving the rectangle

--- main.py
+++ main.py
@@ -21,11 +21,22 @@ rect_x = screen_width / 2 - rect_width / 2
 rect_y = screen_height / 2 - rect_height / 2
 rect_color = pygame.Color('lightyellow')
 
+step = 10
+
 while True:
     for event in pygame.event.get():
         print(event)
         if event.type == pygame.QUIT:
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                rect_y = rect_y - step
+            if event.key == pygame.K_DOWN:
+                rect_y = rect_y + step
+            if event.key == pygame.K_LEFT:
+                rect_x = rect_x - step
+            if event.key == pygame.K_RIGHT:
+                rect_x = rect_x + step
 
     # screen.fill(pygame.Color('blue'))
     # screen.fill((randint(0, 255), randint(0, 255), randint(0, 255)))
