ec6221d750de8819d03b922446f51f7fd64aff38
Darya <ara1015@tut.by>
2025-09-16 16:37:48 +0300

    added methods to a class Alien

--- alien.py
+++ alien.py
@@ -9,3 +9,16 @@ class Alien:
         self.x, self.y = randint(0, SCREEN_WIDTH - self.width), 0
         self.step = ALIEN_STEP
         self.speed = self.step
+
+    def update_position(self):
+        self.y += self.speed
+
+    def increase_speed(self):
+        self.speed += self.step / 2
+
+    def reset(self):
+        self.increase_speed()
+        self.x, self.y = randint(0, SCREEN_WIDTH - self.width), 0
+
+    def has_reached_fighter(self, fighter):
+        return self.y + self.height > fighter.y
