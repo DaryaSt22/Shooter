bb998a6b696449f6df5c4e82c5e17053c8a43f18
Darya <ara1015@tut.by>
2025-09-17 12:37:57 +0300

    adding methods to a class Ball

--- ball.py
+++ ball.py
@@ -10,3 +10,24 @@ class Ball:
         self.stop = BALL_STEP
         self.was_fired = False
         self.fighter = fighter
+
+    def fire(self):
+        self.was_fired = True
+        self.x = self.fighter.x + self.fighter.width / 2 - self.width / 2
+        self.x = self.fighter.y - self.height
+
+    def update_position(self):
+        if self.was_fired:
+            self.y -= self.step
+
+    def is_out_of_screen(self):
+        return self.y + self.height > 0
+
+    def reset(self):
+        self.was_fired = False
+
+    def is_collision(self, alien):
+        return (
+                alien.x < self.x < alien.x + alien.width - self.width and
+                alien.y < self.y < alien.y + alien.height - self.height
+        )
